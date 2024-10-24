08b24deb2282e2ceb43664f66d15630669f2d657
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-10-23 23:14:00 -0300

    Increase background color opacity of Header component for better visibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: rgba(255, 255, 255, 0.20);
+  background-color: rgba(255, 255, 255, 0.25);
   padding: 10px 20px;
   border-radius: 10px;
   z-index: 1;
