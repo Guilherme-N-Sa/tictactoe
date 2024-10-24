873c2723b98040599dbc017547c56410182b1ab1
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-10-24 18:38:11 -0300

    Add Dimmer component for overlay styling

---
+++ src/components/Dimmer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Dimmer = styled.div`
+  background-color: rgba(0, 0, 0, 0.85);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export default Dimmer;
