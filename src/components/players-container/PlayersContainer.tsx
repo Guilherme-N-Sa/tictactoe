f638eecc0ec3bcb57cb80f152afa22f6c87ff8e4
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-10-24 14:49:17 -0300

    Add PlayersContainer component for displaying and managing player data

---
+++ src/components/players-container/PlayersContainer.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Player from "./Player";
+import { IPlayersData } from "./Types";
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 10px;
+
+  @media (max-width: 1600px) {
+    margin-bottom: 5px;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+  }
+`;
+
+interface IPlayersContainerProps {
+  players: IPlayersData;
+  setPlayers: (func: (prev: IPlayersData) => IPlayersData) => void;
+  activePlayer: number;
+}
+
+export default function PlayersContainer({
+  players,
+  setPlayers,
+  activePlayer,
+}: IPlayersContainerProps) {
+  return (
+    <Wrapper>
+      {Object.values(players).map((player) => (
+        <Player key={player.id} setPlayers={setPlayers} player={player} isActive={activePlayer === player.id}/>
+      ))}
+    </Wrapper>
+  );
+}
