1e6c0df77da85d0e5945fff16b37d3e16768005e
GuilhermeSa <guilherme.saddock@olist.com>
2024-10-23 23:04:58 -0300

    Add Header component for game title display

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: rgba(255, 255, 255, 0.25);
+  padding: 10px 20px;
+  border-radius: 10px;
+  z-index: 1;
+  margin-bottom: 40px;
+  font-size: 2rem;
+  color: white;
+`;
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <h1>Tic Tac Toe</h1>
+    </Wrapper>
+  );
+}
