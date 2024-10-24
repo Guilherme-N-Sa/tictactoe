d051c9fa5a7a1b0b82518e134accfbdbec0f4f78
Guilherme-N-Sa <guilherme_niclewicz@hotmail.com>
2024-10-24 18:37:54 -0300

    Refactor App component to integrate game logging and update board state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import PlayersContainer from "./components/players-container/PlayersContainer";
 import ButtonContainer from "./components/ButtonsContainer";
-import GameBoard from "./components/GameBoard";
+import GameBoard from "./components/game-board/GameBoard";
 import Header from "./components/Header";
 import { useState } from "react";
 import { IPlayersData } from "./components/players-container/Types";
+import { ILog } from "./components/game-board/Types";
+import { initialBoard, initialPlayers } from "./constants";
+import checkWinner from "./utils/CheckWinner";
+import Dimmer from "./components/Dimmer";
 
 const Wrapper = styled.div`
   background-image: url("bg.webp");
@@ -17,15 +21,6 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
   position: relative;
-`;
-
-const Overlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.85);
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
 `;
 
 const GameZone = styled.div`
@@ -47,27 +42,25 @@ const GameZone = styled.div`
   }
 `;
 
-const initialBoard = new Array(3).fill(null).map(() => new Array(3).fill(null));
-
 function App() {
-  const [players, setPlayers] = useState<IPlayersData>({
-    1: {
-      id: 1,
-      name: "Player 1",
-      symbol: "X",
-    },
-    2: {
-      id: 2,
-      name: "Player 2",
-      symbol: "O",
-    },
+  const [log, setLog] = useState<ILog[]>([]);
+  const [players, setPlayers] = useState<IPlayersData>(initialPlayers);
+
+  const board = initialBoard.map((row) => [...row]);
+
+  // Determine the active player based on the log
+  let activePlayer = log.length === 0 || log[log.length - 1].player === "X" ? "O" : "X";
+
+  // Update the board based on the log
+  log.forEach(({ player, position }) => {
+    board[position[0]][position[1]] = players[player].symbol;
   });
-  const board = initialBoard;
-  const activePlayer = 1;
+
+  let winner = checkWinner(board);
 
   return (
     <Wrapper>
-      <Overlay />
+      <Dimmer />
       <Header />
       <GameZone>
         <PlayersContainer
@@ -75,8 +68,8 @@ function App() {
           setPlayers={setPlayers}
           activePlayer={activePlayer}
         />
-        <ButtonContainer />
-        <GameBoard board={board} />
+        <ButtonContainer setLog={setLog} />
+        <GameBoard board={board} log={log} setLog={setLog} />
       </GameZone>
     </Wrapper>
   );
